a07b2e20c83b77208bf0a5c46b9c0071a27efc21
Jaka Asa Baldan Ahmad <jakaasha@gmail.com>
2023-12-24 14:33:35 +0700

    11

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,3 +1,39 @@
-export default function Page() {
-  return <p>Invoices Page</p>;
+import { lusitana } from '@/app/ui/fonts';
+import Search from '@/app/ui/search';
+import { CreateInvoice } from './buttons';
+import { Suspense } from 'react';
+import InvoicesTable from './table';
+import { InvoicesMobileSkeleton } from '@/app/ui/skeletons';
+import { fetchInvoicesPages } from '@/app/lib/data';
+import Pagination from './pagination';
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  const query = searchParams?.query || '';
+  const currentPage = Number(searchParams?.page) || 1;
+  const totalPages = await fetchInvoicesPages(query);
+
+  return (
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
+      </div>
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        <Search placeholder="Search invoices..." />
+        <CreateInvoice />
+      </div>
+      <Suspense key={query + currentPage} fallback={<InvoicesMobileSkeleton />}>
+        <InvoicesTable query={query} currentPage={currentPage} />
+      </Suspense>
+      <div className='mt-5 flex w-full justify-center'>
+        <Pagination totalPages={totalPages}/>
+      </div>
+    </div>
+  );
 }
